c28a48d8a1559f53bb42fb72d0969ba2270b2618
Ragnorok303 <jasontompkins22@gmail.com>
2020-02-21 17:42:15 -0700

    started my js code.

---
+++ asset/javascript/game.js
@@ -0,0 +1,99 @@
+var characterSelected = false;
+var defenderSelected = false;
+var character = {};
+var defender = {};
+var enemiesDefeated = 0;
+gameOver = false;
+
+var godzilla = {
+    name: "Godzilla",
+    health: 120,
+    baseAttack: 8,
+    attack: 8,
+};
+
+var mothra = {
+    name: "Mothra",
+    health: 100,
+    baseAttack: 5,
+    attack: 5,
+};
+
+var kingkong = {
+    name: "King Kong",
+    health: 150,
+    baseAttack: 20,
+    attack: 20,
+};
+
+var ghidorah = {
+    name: "Ghidorah",
+    health: 180,
+    baseAttack: 25,
+    attack: 25,
+}
+
+function initializeCharacter(chosenCharacter) {
+    character.name = chosenCharacter.name;
+    character.health = chosenCharacter.health;
+    character.baseAttack = chosenCharacter.baseAttack;
+    character.attack = chosenCharacter.attack;
+}
+
+function initializeDefender(chosenDefender) {
+    defender.name = chosenDefender.name;
+    defender.health = chosenDefender.health;
+    defender.baseAttack = chosenDefender.baseAttack;
+    defender.attack = chosenDefender.attack;
+}
+
+function moveToEnemies() {
+    $(".available-character").removeClass("available-character").addClass("enemy-character");
+    $("#enemies-available").append($(".enemy-character"));
+}
+
+function resetGame() {
+    $("#godzilla-character").children(".health").html(godzilla.health);
+    $("#mothra-character").children(".health").html(mothra.health);
+    $("#king-kong-character").children(".health").html(kingkong.health);
+    $("#ghidorah-character").children(".health").html(ghidorah.health);
+    $(".character-image").removeClass("chosen-character enemy-character defender-character").addClass("available-character");
+    var available = $(".available-character").show();
+    $("#characters-available").html(available);
+    $("#game-message").empty();
+    $("#restart").hide();
+    characterSelected = false;
+    defenderSelected = false;
+    enemiesDefeated = 0;
+    gameOver = false;
+    character = {};
+    defender = {};
+}
+
+$(document).ready(function () {
+
+    $("#restart").hide();
+
+    $("#godzilla-character").on("click", function () {
+        console.log("Godzilla is selected");
+        if (characterSelected == false) {
+            $("#game-message").empty();
+            initializeCharacter(godzilla);
+            characterSelected = true;
+            $("#godzilla-character").removeClass("available-character").addClass("chosen-character");
+            $("#chosen-character").append(this);
+            moveToEnemies();
+        }
+
+        else if ((characterSelected == true) && (defenderSelected == false)) {
+            if ($("#godzilla-character").hasClass("enemy-character")) {
+                $("#game-message").empty();
+                initializeDefender(godzilla);
+                defenderSelected = true;
+                $("#godzilla-character").removeClass("enemy-character").addClass("defender-character");
+                $("#defender-section").append(this);
+            }
+        }
+    });
+
+});
