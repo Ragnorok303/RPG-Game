0065e49684fd94bf53582d1813183eee164fcb47
Ragnorok303 <jasontompkins22@gmail.com>
2020-02-25 14:14:03 -0700

    debugged my page the attack button wasn't working.

--- asset/javascript/game.js
+++ asset/javascript/game.js
@@ -1,87 +1,87 @@
 $(document).ready(function () {
-var characterSelected = false;
-var defenderSelected = false;
-var character = {};
-var defender = {};
-var enemiesDefeated = 0;
-gameOver = false;
-
-var godzilla = {
-    name: "Godzilla",
-    health: 120,
-    baseAttack: 8,
-    attack: 8,
-};
-
-var mothra = {
-    name: "Mothra",
-    health: 100,
-    baseAttack: 5,
-    attack: 5,
-};
-
-var kingkong = {
-    name: "King Kong",
-    health: 150,
-    baseAttack: 20,
-    attack: 20,
-};
-
-var ghidorah = {
-    name: "Ghidorah",
-    health: 180,
-    baseAttack: 25,
-    attack: 25,
-}
-
-function initializeCharacter(chosenCharacter) {
-    character.name = chosenCharacter.name;
-    character.health = chosenCharacter.health;
-    character.baseAttack = chosenCharacter.baseAttack;
-    character.attack = chosenCharacter.attack;
-}
-
-function initializeDefender(chosenDefender) {
-    defender.name = chosenDefender.name;
-    defender.health = chosenDefender.health;
-    defender.baseAttack = chosenDefender.baseAttack;
-    defender.attack = chosenDefender.attack;
-}
-
-function moveToEnemies() {
-    $(".available-character").removeClass("available-character").addClass("enemy-character");
-    $("#enemies-available").append($(".enemy-character"));
-}
-
-function resetGame() {
-    $("#godzilla-character").children(".health").html(godzilla.health);
-    $("#mothra-character").children(".health").html(mothra.health);
-    $("#king-kong-character").children(".health").html(kingkong.health);
-    $("#ghidorah-character").children(".health").html(ghidorah.health);
-    $(".character-image").removeClass("chosen-character enemy-character defender-character").addClass("available-character");
-    var available = $(".available-character").show();
-    $("#characters-available").html(available);
-    $("#game-message").empty();
-    $("#restart").hide();
-    characterSelected = false;
-    defenderSelected = false;
-    enemiesDefeated = 0;
+    var characterSelected = false;
+    var defenderSelected = false;
+    var character = {};
+    var defender = {};
+    var enemiesDefeated = 0;
     gameOver = false;
-    character = {};
-    defender = {};
-}
-var godzillamp3 = document.getElementById("Godzilla");
-var mothramp3 = document.getElementById("Mothra");
-var kingKongmp3 = document.getElementById("King-Kong");
-var ghidorahmp3 = document.getElementById("Ghidorah");
 
-function playAudio(x) {
-  x.play();
-  console.log("im playing");
-}
+    var godzilla = {
+        name: "Godzilla",
+        health: 120,
+        baseAttack: 8,
+        attack: 8,
+    };
+
+    var mothra = {
+        name: "Mothra",
+        health: 100,
+        baseAttack: 5,
+        attack: 5,
+    };
+
+    var kingkong = {
+        name: "King Kong",
+        health: 150,
+        baseAttack: 20,
+        attack: 20,
+    };
+
+    var ghidorah = {
+        name: "Ghidorah",
+        health: 180,
+        baseAttack: 25,
+        attack: 25,
+    }
+
+    function initializeCharacter(chosenCharacter) {
+        character.name = chosenCharacter.name;
+        character.health = chosenCharacter.health;
+        character.baseAttack = chosenCharacter.baseAttack;
+        character.attack = chosenCharacter.attack;
+    }
+
+    function initializeDefender(chosenDefender) {
+        defender.name = chosenDefender.name;
+        defender.health = chosenDefender.health;
+        defender.baseAttack = chosenDefender.baseAttack;
+        defender.attack = chosenDefender.attack;
+    }
+
+    function moveToEnemies() {
+        $(".available-character").removeClass("available-character").addClass("enemy-character");
+        $("#enemies-available").append($(".enemy-character"));
+    }
+
+    function resetGame() {
+        $("#godzilla-character").children(".health").html(godzilla.health);
+        $("#mothra-character").children(".health").html(mothra.health);
+        $("#king-kong-character").children(".health").html(kingkong.health);
+        $("#ghidorah-character").children(".health").html(ghidorah.health);
+        $(".character-image").removeClass("chosen-character enemy-character defender-character").addClass("available-character");
+        var available = $(".available-character").show();
+        $("#characters-available").html(available);
+        $("#game-message").empty();
+        $("#restart").hide();
+        characterSelected = false;
+        defenderSelected = false;
+        enemiesDefeated = 0;
+        gameOver = false;
+        character = {};
+        defender = {};
+    }
+    var godzillamp3 = document.getElementById("monster");
+    var mothramp3 = document.getElementById("moth");
+    var kingKongmp3 = document.getElementById("gorilla");
+    var ghidorahmp3 = document.getElementById("hydra");
+
+    function playAudio(x) {
+        x.play();
+        console.log("im playing");
+    }
 
     $("#restart").hide();
-   
+
     $("#godzilla-character").on("click", function () {
         console.log("Godzilla is selected");
         if (characterSelected == false) {
@@ -174,52 +174,53 @@ function playAudio(x) {
             }
         }
     });
-});
 
-$("#attack").on("click", function () {
-    console.log("Attack selected");
 
-    console.log("character = " + JSON.stringify(character));
-    console.log("defender = " + JSON.stringify(defender));
-    if (characterSelected && defenderSelected && !gameOver) {
-        defender.health = defender.health - character.attack;
-        $(".defender-character").children(".health").html(defender.health);
-        $("#game-message").html("<p>You attacked " + defender.name + " for " + character.attack + " damage.<p>");
-        character.attack = character.attack + character.baseAttack;
-        if (defender.health > 0) {
-            character.health = character.health - defender.baseAttack;
-            $(".chosen-character").children(".health").html(character.health);
-            if (character.health > 0) {
-                $("#game-message").append("<p>" + defender.name + " attacked you back for " + defender.baseAttack + " damage.</p>");
-            } else {
-                gameOver = true;
-                $("#game-message").html("<p>You were defeated...</p><p>Play again?</p>");
-                $("#restart").show();
+    $("#attack").on("click", function () {
+        console.log("Attack selected");
+
+        console.log("character = " + JSON.stringify(character));
+        console.log("defender = " + JSON.stringify(defender));
+        if (characterSelected && defenderSelected && !gameOver) {
+            defender.health = defender.health - character.attack;
+            $(".defender-character").children(".health").html(defender.health);
+            $("#game-message").html("<p>You attacked " + defender.name + " for " + character.attack + " damage.<p>");
+            character.attack = character.attack + character.baseAttack;
+            if (defender.health > 0) {
+                character.health = character.health - defender.baseAttack;
+                $(".chosen-character").children(".health").html(character.health);
+                if (character.health > 0) {
+                    $("#game-message").append("<p>" + defender.name + " attacked you back for " + defender.baseAttack + " damage.</p>");
+                } else {
+                    gameOver = true;
+                    $("#game-message").html("<p>You were defeated...</p><p>Play again?</p>");
+                    $("#restart").show();
+                }
             }
-        }
-        else {
-            enemiesDefeated++;
-            defenderSelected = false;
-            $("#game-message").html("<p>You have defeated " + defender.name + ". Choose another enemy.</p>");
-            $(".defender-character").hide();
-            if (enemiesDefeated === 3) {
-                gameOver = true;
-                $("#game-message").html("<p>You have won the game!!!</p><p>Play again?</p>");
-                $("#restart").show();
+            else {
+                enemiesDefeated++;
+                defenderSelected = false;
+                $("#game-message").html("<p>You have defeated " + defender.name + ". Choose another enemy.</p>");
+                $(".defender-character").hide();
+                if (enemiesDefeated === 3) {
+                    gameOver = true;
+                    $("#game-message").html("<p>You have won the game!!!</p><p>Play again?</p>");
+                    $("#restart").show();
+                }
             }
+        } else if (!characterSelected && !gameOver) {
+            $("#game-message").html("<p>You must first select your game character.</p>");
+        } else if (!defenderSelected && !gameOver) {
+            $("#game-message").html("<p>You must choose an enemy to fight.</p>");
         }
-    } else if (!characterSelected && !gameOver) {
-        $("#game-message").html("<p>You must first select your game character.</p>");
-    } else if (!defenderSelected && !gameOver) {
-        $("#game-message").html("<p>You must choose an enemy to fight.</p>");
-    }
 
-    console.log("character = " + JSON.stringify(character));
-    console.log("defender = " + JSON.stringify(defender));
-});
+        console.log("character = " + JSON.stringify(character));
+        console.log("defender = " + JSON.stringify(defender));
+    });
 
-$("#restart").on("click", function () {
-    console.log("Restart selected");
+    $("#restart").on("click", function () {
+        console.log("Restart selected");
 
-    resetGame();
+        resetGame();
+    });
 });
